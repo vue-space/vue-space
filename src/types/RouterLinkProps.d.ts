c86e3b666bae337b4644ee548a7981c63dc26be0
Kunduin <15852158126@163.com>
2019-10-19 11:47:13 +0800

    chore(types): add aria attributes & define router link

---
+++ src/types/RouterLinkProps.d.ts
@@ -0,0 +1,9 @@
+export type RouterLinkProps = {
+  to?: string | object;
+  tag?: string;
+  exact?: boolean;
+  append?: boolean;
+  replace?: boolean;
+  activeClass?: string;
+  exactActiveClass: string;
+};
