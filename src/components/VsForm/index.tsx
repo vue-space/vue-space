c6013aaa1834a48b1a37e4aaf701f68bee225f62
Kunduin <15852158126@163.com>
2019-10-28 08:31:17 +0800

    feat(form): add form components

---
+++ src/components/VsForm/index.tsx
@@ -0,0 +1,15 @@
+import { createComponent } from "@vue/composition-api";
+import { VNode } from "vue";
+import { FormHTMLAttributes } from "../../types/dom";
+
+// eslint-disable-next-line @typescript-eslint/no-empty-interface
+interface VsFormProps extends FormHTMLAttributes {}
+
+const VsForm = createComponent<VsFormProps>({
+  props: {},
+  setup(props, ctx) {
+    return (): VNode => <form>{ctx.slots.default()}</form>;
+  }
+});
+
+export default VsForm;
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,9 @@ import VsButton from "./components/VsButton";
 import VsThemeProvider from "./components/VsThemeProvider";
 import VsCircularProgress from "./components/VsCircularProgress";
 import VsInput from "./components/VsInput";
+import VsIcon from "./components/VsIcon";
+import VsForm from "./components/VsForm";
+import VsFormItem from "./components/VsFormItem";
 import VueCompositionApi from "@vue/composition-api";
 
 import { PluginFunction } from "vue";
@@ -10,7 +13,10 @@ const Components = {
   VsButton,
   VsThemeProvider,
   VsCircularProgress,
-  VsInput
+  VsInput,
+  VsIcon,
+  VsForm,
+  VsFormItem
 };
 
 const install: PluginFunction<never> = function(Vue) {
