26d263b4dc896c233c103c6b167362c365019535
Kunduin <15852158126@163.com>
2019-12-14 23:46:11 +0800

    test(input): add more input test with validate
    affects: @vue-space/storybook

--- storybook/.storybook/config.js
+++ storybook/.storybook/config.js
@@ -3,8 +3,17 @@ import { DocsPage, DocsContainer } from '@storybook/addon-docs/blocks';
 import Vue from 'vue';
 import VueSpace from '@vue-space/core';
 import '@vue-space/styles';
-
+import { ValidationProvider, extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
 Vue.use(VueSpace);
+
+Vue.component('ValidationProvider', ValidationProvider);
+
+// No message specified.
+extend('email', email);
+
+// Override the default message.
+extend('required', required);
 
 addParameters({
   docs: {
