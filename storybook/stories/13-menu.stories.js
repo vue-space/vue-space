ca30a63cdfd60034d5fd44330538d64e8783819e
Kunduin <15852158126@163.com>
2019-12-22 11:41:18 +0800

    test(icons): use new version icons build for story
    affects: @vue-space/storybook

--- storybook/stories/13-menu.stories.js
+++ storybook/stories/13-menu.stories.js
@@ -1,41 +1,45 @@
-import { html } from "./utils/editor";
-import { paddingDecorator } from "./utils/decorators";
-import { withKnobs } from "@storybook/addon-knobs";
+import { html } from './utils/editor';
+import { paddingDecorator } from './utils/decorators';
+import { withKnobs } from '@storybook/addon-knobs';
+import VsIconCube from '@vue-space/icons/dist/VsIconCube';
 
 export default {
-  title: "vs-menu",
+  title: 'vs-menu',
   decorators: [withKnobs, paddingDecorator]
 };
 
 export const allPropsInteractive = () => ({
+  components: {
+    VsIconCube
+  },
   template: html`
     <vs-menu style="max-width:220px">
       <vs-menu-item :caption="true">基本选项</vs-menu-item>
       <vs-menu-item v-slot="{ activeClass }">
-        <a :class="activeClass"><vs-icon name="cube" />基本选项1</a>
+        <a :class="activeClass"><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
 
       <vs-menu-item :caption="true">基本选项</vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
       <vs-menu-item>
-        <a><vs-icon name="cube" />基本选项1</a>
+        <a><vs-icon-cube />基本选项1</a>
       </vs-menu-item>
     </vs-menu>
   `
